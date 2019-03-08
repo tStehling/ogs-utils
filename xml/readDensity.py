1c140d5f8479aff303883a1ea9b41077c4c97280
Thomas Fischer <thomas.fischer@ufz.de>
2019-03-08 15:11:21 +0100

    wip add writeAsMediumPropertyToFile 2019-03-08

--- xml/readDensity.py
+++ xml/readDensity.py
@@ -10,15 +10,28 @@ class ConstantProperty:
         self.xml_path = "//OpenGeoSysProject/media/medium/phases/phase/properties"
 
     def writeAsMediumPropertyToFile(self, file_name):
+        print("writeAsMediumPropertyToFile ...")
         ex = "xmlstarlet ed "
         path = self.xml_path
         node = "property_density"
-        node_cmd = ex + " --subnode \"" + path + "\" --type elem -n \"node\"" + file_name + " > /tmp/2.prj"
+        node_cmd = ex + " --subnode \"" + path + "\" --type elem -n \"node\"" + file_name
         process = subprocess.Popen(node_cmd.split(), stdout=subprocess.PIPE)
+        output_stream, errors = process.communicate()
+        output = output_stream.decode()
+        outfile = open('/tmp/2.prj', 'w')
+        outfile.write(output)
+        exit()
+
         ppath = path + "/" + node
         subnode_cmd = ex + " --subnode \"" + ppath + "\" --type elem"
-        name_cmd = subnode_cmd + " -n \"name\" -v \"density\"" + "/tmp/2.prj  > /tmp/3.prj"
+        name_cmd = subnode_cmd + " -n \"name\" -v \"density\"" + "/tmp/2.prj"
         process = subprocess.Popen(name_cmd.split(), stdout=subprocess.PIPE)
+        process = subprocess.Popen(node_cmd.split(), stdout=subprocess.PIPE)
+        output_stream, errors = process.communicate()
+        output = output_stream.decode()
+        outfile = open('/tmp/3.prj', 'w')
+        outfile.write(output)
+
         type_cmd = subnode_cmd + " -n \"type\" -v \"Constant\" /tmp/3.prj  > /tmp/4.prj"
         process = subprocess.Popen(type_cmd.split(), stdout=subprocess.PIPE)
         value_cmd = subnode_cmd + " -n \"value\" -v \"" + str(value) + "\" /tmp/4.prj  > /tmp/5.prj"
@@ -36,12 +49,20 @@ class LinearProperty:
         self.xml_path = "//OpenGeoSysProject/media/medium/phases/phase/properties"
 
     def writeAsMediumPropertyToFile(self, file_name):
+        print("LinearProperty: writeAsMediumPropertyToFile ...")
         ex = "xmlstarlet ed "
         path = self.xml_path
         node = "property_density"
-        node_cmd = ex + " --subnode \"" + path + "\" --type elem -n \"node\"" + file_name + " > /tmp/2.prj"
+        node_cmd = ex + " --subnode " + path + " --type elem -n " + node + " " + file_name
         print(node_cmd)
         process = subprocess.Popen(node_cmd.split(), stdout=subprocess.PIPE)
+        output_stream, errors = process.communicate()
+        output = output_stream.decode()
+        outfile = open('/tmp/2.prj', 'w')
+        outfile.write(output)
+
+        exit()
+
         ppath = path + "/" + node
         subnode_cmd = ex + " --subnode \"" + ppath + "\" --type elem"
         name_cmd = subnode_cmd + " -n \"name\" -v \"density\"" + "/tmp/2.prj  > /tmp/3.prj"
@@ -67,35 +88,78 @@ class LinearProperty:
         rename_cmd = ex + " --rename \"" + ppath + "\" -v \"property\" /tmp/9.prj  > " + file_name
         process = subprocess.Popen(rename_cmd.split(), stdout=subprocess.PIPE)
 
-def addMedia(in, out):
+def addMedia(input, out):
     xml_path = "//OpenGeoSysProject/time_loop"
-    ex = "xmlstarlet ed --insert " + xml_path + " --type elem -n \"media\" " + in + " > " + out
+    ex = "xmlstarlet ed --insert " + xml_path + " --type elem -n media " + input
     process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
+    output_stream, errors = process.communicate()
+    output = output_stream.decode()
+    outfile = open(out, 'w')
+    outfile.write(output)
 
-def addMedium(in, out, medium_id):
+def addMedium(input, out, medium_id):
     xml_path = "//OpenGeoSysProject/media"
-    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n \"medium\" " + in + " >  /tmp/tmp_medium_out.prj"
+    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n medium " + input
     process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
+    output_stream, errors = process.communicate()
+    output = output_stream.decode()
+    outfile = open('/tmp/temp_medium_out.prj', 'w')
+    outfile.write(output)
 
     xml_path = xml_path + "/medium"
-    ex = "xmlstarlet ed --subnode " + xml_path + " --type attr -n \"id\" -v " + str(medium_id) + " /tmp/tmp_medium_out.prj > " + out
+    ex = "xmlstarlet ed --subnode " + xml_path + " --type attr -n id -v " + str(medium_id) + " /tmp/temp_medium_out.prj"
     process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
+    output_stream, errors = process.communicate()
+    output = output_stream.decode()
+    outfile = open(out, 'w')
+    outfile.write(output)
 
 
-def addPhases(in, out):
+def addPhases(input, out):
     xml_path = "//OpenGeoSysProject/media/medium"
-    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n \"phases\" " + in + " > " + out
+    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n phases " + input
     process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
+    output_stream, errors = process.communicate()
+    output = output_stream.decode()
+    outfile = open(out, 'w')
+    outfile.write(output)
 
-def addPhase(in, out, phase):
+def addPhase(input, out, phase):
     xml_path = "//OpenGeoSysProject/media/medium/phases"
-    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n \"phase\" " + in + " > " + out
+    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n phase " + input
     process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
-    # AqueousLiquid
-    xml_path = "//OpenGeoSysProject/media/medium/phases/phase"
-    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n \"AqueousLiquid\" " + in + " > " + out
-    process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
+    output_stream, errors = process.communicate()
+    output = output_stream.decode()
+    outfile = open(out, 'w')
+    outfile.write(output)
 
+    # phase
+    xml_path = "//OpenGeoSysProject/media/medium/phases/phase"
+    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n type -v " + phase + " " + out
+    process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
+    output_stream, errors = process.communicate()
+    output = output_stream.decode()
+    outfile = open(out, 'w')
+    outfile.write(output)
+
+def addProperties(input, out, phase):
+    xml_path = "//OpenGeoSysProject/media/medium/phases/phase"
+    ex = "xmlstarlet ed --subnode " + xml_path + " --type elem -n properties " + input
+    process = subprocess.Popen(ex.split(), stdout=subprocess.PIPE)
+    output_stream, errors = process.communicate()
+    output = output_stream.decode()
+    outfile = open(out, 'w')
+    outfile.write(output)
+
+### create media xml tree
+addMedia(sys.argv[1], '/tmp/media.prj')
+addMedium('/tmp/media.prj', '/tmp/medium.prj', 0)
+addPhases('/tmp/medium.prj', '/tmp/phases.prj')
+addPhase('/tmp/phases.prj', '/tmp/phase.prj', 'AqueousLiquid')
+addProperties('/tmp/phase.prj', '/tmp/properties.prj', 'AqueousLiquid')
+# rename
+rename='cp /tmp/properties.prj ' + sys.argv[1]
+rename_process = subprocess.Popen(rename.split(), stdout=subprocess.PIPE)
 
 ### read the density
 # read property type: ConstantProperty or LinearProperty
