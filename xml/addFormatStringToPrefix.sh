612503aab486040698412a077ee19aa4900b91be
Thomas Fischer <thomas.fischer@ufz.de>
2020-05-05 19:37:41 +0200

    Update the addFormatStringToPrefix.sh script.

--- xml/addFormatStringToPrefix.sh
+++ xml/addFormatStringToPrefix.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-sub_path=//OpenGeoSysProject/time_loop/output/prefix
+base_path=//OpenGeoSysProject/time_loop/output
+sub_path=${base_path}/prefix
 prefix_value=`xmlstarlet sel -t -v $sub_path $1`
-prefix_format_string=${prefix_value}_pcs_{:process}_ts_{:timestep}_t_{:time}
+prefix_format_string=${prefix_value}_pcs_{:process_id}
+suffix_format_string=_ts_{:timestep}_t_{:time}
 
 xmlstarlet ed -u $sub_path -v $prefix_format_string $1 > /tmp/prefix.prj
+xmlstarlet ed -s ${base_path} --type elem -n suffix -v $suffix_format_string /tmp/prefix.prj > /tmp/suffix.prj
 
 #format the result file
-xmlstarlet fo -s 4 /tmp/prefix.prj > ${1}
+xmlstarlet fo -s 4 /tmp/suffix.prj > ${1}
